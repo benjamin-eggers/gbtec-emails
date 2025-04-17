cb78564e0273c08f880f0613e72dba46120572b3
Benjamin Eggers <benjamin@space22.de>
2025-04-17 14:32:17 +0200

    🎉 Initial commit
    - Setup project with start.spring.io

---
+++ src/test/java/de/beg/gbtec/emails/ApplicationTest.java
@@ -0,0 +1,13 @@
+package de.beg.gbtec.emails;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
